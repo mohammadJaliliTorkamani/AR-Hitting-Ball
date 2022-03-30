a76d109d7aed4d2ea5845d1b13e3a3b427cf03b5
mohammad <mohammad.jalili.torkamani@gmail.com>
2022-03-30 13:03:00 +0430

    circular ball

--- Utils/Drawer.py
+++ Utils/Drawer.py
@@ -8,6 +8,10 @@ class Drawer:
 
     _PIXEL_DIMENSION = 10
 
+    _BLOCK_COLOR = (255, 0, 0)
+    _BALL_COLOR = (0, 0, 255)
+    _SURFACE_COLOR = (250, 253, 15)
+
     def __init__(self):
         self._mask = {}
         self.output = None
@@ -19,10 +23,16 @@ class Drawer:
     def blend(self, cv_img):
         self.output = cv_img
         for (point, value) in self._mask.items():
-            color = (250, 253, 15) if value == 0 else ((0, 0, 255) if value == 1 else (255, 0, 0))
-            start_pos = (point[0] - int(Drawer._PIXEL_DIMENSION / 2), point[1] - int(Drawer._PIXEL_DIMENSION / 2))
-            end_pos = (point[0] + int(Drawer._PIXEL_DIMENSION / 2), point[1] + int(Drawer._PIXEL_DIMENSION / 2))
-            cv2.rectangle(self.output, start_pos, end_pos, color, cv2.FILLED)
+            color = Drawer._SURFACE_COLOR if value == Drawer.SURFACE_DRAWING else (
+                Drawer._BALL_COLOR if value == Drawer.BALL_DRAWING else Drawer._BLOCK_COLOR)
+
+            if (value == Drawer.BALL_DRAWING) or (value == Drawer.SURFACE_DRAWING):
+                cv2.circle(self.output, point, Drawer._PIXEL_DIMENSION, color, cv2.FILLED)
+            else:
+                start_pos = (point[0] - int(Drawer._PIXEL_DIMENSION / 2), point[1] - int(Drawer._PIXEL_DIMENSION / 2))
+                end_pos = (point[0] + int(Drawer._PIXEL_DIMENSION / 2), point[1] + int(Drawer._PIXEL_DIMENSION / 2))
+                cv2.rectangle(self.output, start_pos, end_pos, color, cv2.FILLED)
+
 
     def clear(self, pos):
         if pos in self._mask:
