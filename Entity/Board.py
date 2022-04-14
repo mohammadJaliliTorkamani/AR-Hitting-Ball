b684b20e05bffcfb5a51509d856a04d3e6b36f97
mohammad <mohammad.jalili.torkamani@gmail.com>
2022-04-15 00:22:10 +0430

    colorful blocks were added

--- Entity/Board.py
+++ Entity/Board.py
@@ -1,3 +1,5 @@
+import random
+
 from Entity.Block import Block
 from Utils import Constants
 
@@ -5,5 +7,11 @@ from Utils import Constants
 class Board:
     def __init__(self, board_size):
         self.size = board_size
-        self.blocks = [[Block((j + 1) + (i * self.size[1]), (i + 1, j + 1),Constants.BLOCK_COLOR)
-                        for j in range(self.size[1])] for i in range(self.size[0])]
+        self.blocks = []
+        for i in range(self.size[0]):
+            row = []
+            for j in range(self.size[1]):
+                block_color = Constants.BLOCK_COLORS[random.randint(0,len(Constants.BLOCK_COLORS)-1)]
+                row.append(Block((j + 1) + (i * self.size[1]), (i + 1, j + 1),block_color))
+            self.blocks.append(row)
+
